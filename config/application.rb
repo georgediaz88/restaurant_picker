ae759a5b092263f8f49ebb151c31bb240d12df75
George Diaz <georgediaz88@yahoo.com>
2015-02-16 18:09:51 -0600

    Be explicit to precompile

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,17 @@ module RestaurantPicker
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml', '.slim',        # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
